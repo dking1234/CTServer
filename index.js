07cc5df9507f30b2728f0ed376e9e3a9df3536a3
dking1234 <39795989+dking1234@users.noreply.github.com>
2023-12-17 19:54:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const bookingRoutes = require('./Booking/bookingRoutes');
 const seatController = require('./Seats/seatsController');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
 
